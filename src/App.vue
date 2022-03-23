adbd56b3117a561098ee4fa8664b0a15927bf6d7
Jonas <jonas.pfammatter@lernende.bfo-vs.ch>
2022-03-23 11:53:28 +0100

    API connection funktioiert nicht

--- src/App.vue
+++ src/App.vue
@@ -43,26 +43,22 @@ export default {
     return {
       newTask: "",
       NameCategory: "test",
-      cTasks: [],
       categoryName: "Hallo",
       todo:"",
       categories: [],
       newCategory: "",
       
       newTL:"",
-      tasklists: [{id:1, 
-                  newTask: "",
-                  categoryName:"hello", 
-                  tasks:[{id:1, name: "hello"}]
-                  }]
+      tasklists: [[]]
+                  
 
 
     };
   },
    async created() {
     try {
-      const res = await axios.get(`http://localhost:3000/todos`);
-      this.cTasks = res.data;
+      const res = await axios.get(`http://localhost:3000/tasklists`);
+      this.tasklists = res.data;
     } catch (error) {
       console.log(error);
     }
@@ -73,36 +69,34 @@ export default {
  async submitTask() {
     let new_id;
     if(this.newTask=="") return;
-    if(this.cTasks.lenght){
-        new_id = this.cTasks.slice(-1)[0].id + 1;}
+    if(this.tasklists.lenght){
+        new_id = this.tasklists.slice(-1)[0].id + 1;}
     else { new_id = 1; }
-      const res = await axios.post(`http://localhost:3000/todos`, {
+      const res = await axios.post(`http://localhost:3000/tasklists`, {
         id: new_id,
-        name: this.newTask,
+        name: this.newTask
       });
-      this.cTasks = [...this.cTasks, res.data];
+      this.tasklists = [...this.tasklists, res.data];
 
      /*  
-      this.cTasks.push({
+      this.tasklists.push({
         id: new_id,
         name: this.newTask,
         */
      },
      removeTask(id) {
-    axios.delete(`http://localhost:3000/todos/${id}`)
-    this.cTasks = this.cTasks.filter(cTasks => cTasks.id !== id)
+    axios.delete(`http://localhost:3000/tasklists/${id}`)
+    this.tasklists = this.tasklists.filter(tasklists => tasklists.id !== id)
     },
-
-    createTL: function createTL() {
+ async createTL() {
     if(this.newTL=="") return;
-    let new_id = this.tasklists.slice(-1)[0].id + 1;
+    let new_id_box = this.tasklists.slice(-1)[0].id + 1;
    //  console.log(this.newTask);
-      this.tasklists.push({
-        id: new_id,
-        categoryName: this.newTL,
-        tasks:[]
-        
+     const res = await axios.post(`http://localhost:3000/tasklists`, {
+        id: new_id_box,
+        categoryName: this.newTL
       });
+      this.tasklists = [...this.tasklists, res.data];
       this.newTL = "";
    },
 
@@ -114,7 +108,7 @@ export default {
       const res = await axios.post(`http://localhost:3000/todos`, {
         name: this.newTask,
       });
-      this.cTasks = [...this.cTasks, res.data];
+      this.tasklists = [...this.tasklists, res.data];
       this.newTask = "";
     },*/
 
